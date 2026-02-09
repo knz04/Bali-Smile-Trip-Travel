4bd55bde02bbe11a26fec17f75d34645a9db1fc8
hawyeet <53439203+knz04@users.noreply.github.com>
2026-02-09 13:05:46 +0700

    chore: initialize tailwind and prettier, feat: add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+interface ButtonProps {
+  variant?: "primary" | "secondary";
+  children?: React.ReactNode;
+}
+
+export default function Button({
+  variant = "secondary",
+  children = "Book Now",
+}: ButtonProps) {
+  return (
+    <div
+      className={`w-fit rounded-sm border-2 px-8 py-2 transition duration-300 ease-in-out hover:cursor-pointer ${
+        variant === "primary"
+          ? "bg-yellow hover:border-yellow hover:bg-background border-transparent"
+          : "border-yellow hover:bg-yellow bg-background"
+      }`}
+    >
+      <span className="text-primary text-center text-xl font-normal">
+        {children}
+      </span>
+    </div>
+  );
+}
