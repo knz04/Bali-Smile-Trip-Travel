1e8e7df7a5405f0f60772c443d59d599d5f1cc7b
hawyeet <53439203+knz04@users.noreply.github.com>
2026-02-16 13:27:58 +0700

    feat: add home hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,33 @@
+import heroImage from "../assets/hero-home.webp";
+import Button from "./Button";
+
+export default function Hero() {
+  return (
+    <div className="relative h-[calc(100vh-72px)] lg:h-[calc(100vh-104px)]">
+      <img
+        src={heroImage}
+        alt="Hero Image"
+        className="absolute h-full w-full object-cover brightness-80"
+      />
+      <div className="relative flex h-full flex-col items-center justify-center gap-y-2">
+        <p className="text-background text-center text-6xl font-semibold">
+          Private Bali Tours & Certified Drivers.
+        </p>
+        <p className="text-background text-center text-6xl font-semibold">
+          Explore Bali with Comfort.
+        </p>
+        <p className="text-background text-center text-4xl font-normal">
+          Exploring Destinations, Creating Memories.
+        </p>
+        <p className="text-background text-center text-base font-light">
+          Your trusted Bali tour and transport provider, where every journey is
+          crafted with heart, passion, and true Balinese hospitality.
+        </p>
+        <div className="flex flex-row items-center justify-center gap-x-4 pt-2">
+          <Button>Book Your Tour</Button>
+          <Button>View Our Packages</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
