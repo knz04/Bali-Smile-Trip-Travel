f0dec5f142b2ccf013afbeca721c4afd5febab3c
hawyeet <53439203+knz04@users.noreply.github.com>
2026-02-16 13:44:38 +0700

    feat: add mobile welcome section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import Hero from "./components/Hero";
 
 import Navbar from "./components/Navbar";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Welcome />
     </>
   );
 }
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -3,19 +3,19 @@ import welcome from "../assets/welcome.webp";
 
 export default function Welcome() {
   return (
-    <div className="flex h-[80vh] w-full flex-row items-center justify-center gap-x-8">
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-y-8 py-2 lg:h-[80vh] lg:flex-row lg:gap-x-8">
       <div className="flex">
         <img src={welcome} alt="Welcome" className="h-96 w-full" />
       </div>
-      <div className="flex w-[40%] flex-col gap-y-4">
+      <div className="flex w-[90%] flex-col gap-y-4 lg:w-[40%]">
         <p className="text-4xl">Welcome to Bali Smile Trip & Travel</p>
-        <p className="max-w-[75%] pt-2 font-light">
+        <p className="pt-2 font-light lg:max-w-[75%]">
           Founded by Dewa Wahya, a dedicated Balinese local with many years of
           experience in the tourism and hospitality industry, our mission is
           simple: to create personal, meaningful, and unforgettable travel
           experiences in Bali.
         </p>
-        <p className="max-w-[75%] pb-2 font-light">
+        <p className="pb-2 font-light lg:max-w-[75%]">
           We go beyond standard transportation — we share stories, insights, and
           the authentic essence of Bali during every trip.
         </p>
