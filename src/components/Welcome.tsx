a8a5b37f954f2bab4d9ebc1b9bc8654676adab93
hawyeet <53439203+knz04@users.noreply.github.com>
2026-02-19 16:12:45 +0700

    fix: welcome sm view

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -3,19 +3,21 @@ import welcome from "../assets/welcome.webp";
 
 export default function Welcome() {
   return (
-    <div className="flex h-fit w-full flex-col items-center justify-center gap-y-8 py-2 lg:h-[50vh] lg:flex-row lg:gap-x-8">
+    <div className="flex w-full flex-col items-center justify-center gap-y-8 py-2 sm:h-screen lg:h-[50vh] lg:flex-row lg:gap-x-8">
       <div className="flex">
-        <img src={welcome} alt="Welcome" className="h-96 w-full" />
+        <img src={welcome} alt="Welcome" className="w-full sm:h-64 lg:h-96" />
       </div>
       <div className="flex w-[90%] flex-col gap-y-4 lg:w-[40%]">
-        <p className="text-4xl">Welcome to Bali Smile Trip & Travel</p>
-        <p className="pt-2 font-light lg:max-w-[75%]">
+        <p className="text-start sm:text-3xl lg:text-4xl">
+          Welcome to Bali Smile Trip & Travel
+        </p>
+        <p className="pt-2 font-light sm:text-sm lg:max-w-[75%] lg:text-base">
           Founded by Dewa Wahya, a dedicated Balinese local with many years of
           experience in the tourism and hospitality industry, our mission is
           simple: to create personal, meaningful, and unforgettable travel
           experiences in Bali.
         </p>
-        <p className="pb-2 font-light lg:max-w-[75%]">
+        <p className="pt-2 font-light sm:text-sm lg:max-w-[75%] lg:text-base">
           We go beyond standard transportation — we share stories, insights, and
           the authentic essence of Bali during every trip.
         </p>
