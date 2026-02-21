b672fa59620e4251567557fc131474e890c22946
hawyeet <53439203+knz04@users.noreply.github.com>
2026-02-21 10:51:07 +0700

    fix: welcome lg view

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -3,12 +3,12 @@ import welcome from "../assets/welcome.webp";
 
 export default function Welcome() {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-y-8 py-2 sm:h-[60vh] lg:h-[50vh] lg:flex-row lg:gap-x-8">
+    <div className="flex w-full flex-col items-center justify-center gap-y-8 py-2 sm:h-[60vh] lg:h-screen lg:flex-row lg:gap-x-8 xl:h-[50vh]">
       <div className="flex">
         <img src={welcome} alt="Welcome" className="w-full sm:h-64 lg:h-96" />
       </div>
       <div className="flex w-[90%] flex-col gap-y-4 lg:w-[40%]">
-        <p className="text-start sm:text-3xl lg:text-4xl">
+        <p className="text-start sm:text-3xl xl:text-4xl">
           Welcome to Bali Smile Trip & Travel
         </p>
         <p className="pt-2 font-light sm:text-sm lg:max-w-[75%] lg:text-base">
