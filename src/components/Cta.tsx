5933390dc559511545ec8fd3e5ee593893b2519d
hawyeet <53439203+knz04@users.noreply.github.com>
2026-02-21 10:39:21 +0700

    fix: cta sm view

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -3,7 +3,7 @@ import ctaImage from "../assets/cta.webp";
 
 export default function Cta() {
   return (
-    <div className="relative flex h-fit items-center justify-center py-12 lg:h-[24vh] lg:p-0">
+    <div className="relative flex items-center justify-center py-12 sm:h-fit lg:h-[24vh] lg:p-0">
       <img
         src={ctaImage}
         alt="CTA Image"
@@ -11,10 +11,10 @@ export default function Cta() {
       />
       <div className="relative flex w-full flex-col items-center justify-center gap-y-4 lg:w-[80%] lg:flex-row lg:justify-around lg:gap-0">
         <div className="flex w-[90%] flex-col items-center gap-y-2 lg:w-[50%] lg:items-start">
-          <p className="text-background text-center text-4xl lg:text-left">
+          <p className="text-background text-center sm:text-3xl lg:text-left lg:text-4xl">
             Let Us Be Your Local Friend in Bali
           </p>
-          <p className="text-background w-full text-center text-lg font-light lg:w-[60%] lg:text-left">
+          <p className="text-background w-full text-center font-light sm:text-base lg:w-[60%] lg:text-left lg:text-lg">
             Whether you seek romance, family bonding, luxury comfort, or
             adventurous exploration, Bali Smile Trip & Travel is here to create
             a journey you’ll remember forever.
