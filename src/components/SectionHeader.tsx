6afac6abc9e5eaa77cbc8b1796c5bb5fd48656ff
hawyeet <53439203+knz04@users.noreply.github.com>
2026-02-24 15:33:34 +0700

    feat: about page md view

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -10,15 +10,15 @@ export default function SectionHeader({
   subheading,
 }: SectionHeaderProps) {
   return (
-    <div className="relative lg:h-[40vh]">
+    <div className="relative md:h-[24vh] lg:h-[40vh]">
       <img
         src={header}
         alt="Header"
         className="absolute h-full w-full object-cover brightness-50"
       />
       <div className="relative flex h-full flex-col items-center justify-center gap-y-2">
-        <p className="text-background text-center lg:text-3xl">{heading}</p>
-        <p className="text-background w-[50%] text-center font-light lg:text-base">
+        <p className="text-background text-center md:text-3xl">{heading}</p>
+        <p className="text-background w-[80%] text-center text-sm font-light lg:w-[50%] lg:text-base">
           {subheading}
         </p>
       </div>
